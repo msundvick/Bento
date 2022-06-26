3840d4bb7a0921e98f42db11685d1e1d38e5bffd
Michael Sundvick <msundvick@sailgp.com>
2022-06-26 20:26:45 +1200

    Add stuff for pwa, mostly to get offline cache

--- src/main.js
+++ src/main.js
@@ -6,3 +6,9 @@ import {} from "./time";
 
 import { createIcons, icons } from "lucide";
 createIcons({ icons });
+
+import { registerSW } from "virtual:pwa-register";
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 /*eslint-env node */
 /// <reference types="vitest" />
 import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
@@ -9,5 +10,6 @@ export default ({ mode }) => {
   return defineConfig({
     base: "./",
     test: {},
+    plugins: [VitePWA()],
   });
 };
